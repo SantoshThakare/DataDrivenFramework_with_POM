80b26c518332464a1421d2091ef738cf9c2b19f8
SantoshThakare <santoshthakare50@gmail.com>
2022-10-09 22:42:17 +0530

    Added : creating Base Class and Add Load the Properties File

---
+++ SeleniumDataDriven-main/FaceBook_POM_DataDrivenFramework-main/PageObjectModel/src/main/java/base/Base.java
@@ -0,0 +1,64 @@
+package base;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import Utility.utility;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import pages.FBHomePage;
+import pages.FindFriendPage;
+import pages.LoginPage;
+import pages.MarketPlacePage;
+import pages.ProfilePage;
+import pages.UploadProfileVideoPage;
+import pages.WatchVideosPage;
+public class Base {
+
+	protected static WebDriver driver;
+	protected static WebElement element;
+	protected static FileInputStream file;
+	protected static Properties prop;
+	protected static utility util;
+	protected static FileInputStream excelfile;
+	protected static XSSFWorkbook xWorkBook;
+	protected static XSSFSheet xSheet;
+	protected static Object[][] data;
+	protected static LoginPage loginp;
+	protected static String message;
+	protected static FindFriendPage findpage;
+	protected static WatchVideosPage watchvideo;
+	protected static MarketPlacePage marketplace;
+	protected static UploadProfileVideoPage uploadprofilevideo;
+	protected static ProfilePage profile;
+	protected static FBHomePage FBhome;
+	static {
+
+		WebDriverManager.chromedriver().setup();
+	}
+
+
+	protected final static String FILE_PATH = System.getProperty("user.dir") + File.separator + "src" + File.separator
+			+ "main" + File.separator + "resources" + File.separator + "repository" + File.separator
+			+ "LoginTestData.xlsx";
+
+	/**
+	 *  To Load the Properties file in file.io
+	 */
+
+	public Base() {
+		try {
+			prop = new Properties();
+			file = new FileInputStream(
+					".\\src\\main\\resources\\config.properties");
+			prop.load(file);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
