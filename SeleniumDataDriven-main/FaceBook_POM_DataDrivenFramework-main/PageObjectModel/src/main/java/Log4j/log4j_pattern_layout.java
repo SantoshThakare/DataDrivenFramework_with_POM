590064648d8508b4e193e47d872d6c856f8ff3cb
SantoshThakare <santoshthakare50@gmail.com>
2022-10-14 08:00:55 +0530

    Pattern Layout Log4j

--- SeleniumDataDriven-main/FaceBook_POM_DataDrivenFramework-main/PageObjectModel/src/main/java/Log4j/log4j_pattern_layout.java
+++ SeleniumDataDriven-main/FaceBook_POM_DataDrivenFramework-main/PageObjectModel/src/main/java/Log4j/log4j_pattern_layout.java
@@ -7,21 +7,22 @@ import org.apache.log4j.Logger;
 
 public class log4j_pattern_layout {
 
-static{
+	static {
 
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy-hh-mm-ss");
-        System.setProperty("current.date.time", dateFormat.format(new Date()));
-    }
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy-hh-mm-ss");
+		System.setProperty("current.date.time", dateFormat.format(new Date()));
+	}
 
-    final static Logger log = Logger.getLogger(log4j_pattern_layout.class);
+	final static Logger log = Logger.getLogger(log4j_pattern_layout.class);
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        log.trace("This is Trace Message.");
-        log.debug("This is Debug Message.");
-        log.info("This is Info Message.");
-        log.warn("This is Warn Message.");
-        log.error("This is Error Message.");
-        log.fatal("This is Fatal Message.");
-    }
+		log.trace("This is Trace Message.");
+		log.debug("This is Debug Message.");
+		log.info("This is Info Message.");
+		log.warn("This is Warn Message.");
+		log.error("This is Error Message.");
+		log.fatal("This is Fatal Message.");
+	
+	}
 }
